4bba598fb6a89aa1de2f74abef23f452a2c06fdb
dfdellys <dd2feliciano@gmail.com>
2021-10-27 23:53:38 -0500

    Create reading.js

---
+++ helpers/reading.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) => fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => (err ? console.error(err) : console.info(`\nData written to ${destination}`)));
+
+const notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+
+const uuid = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+module.exports = { readFromFile, uuid, notesData, readAndAppend };
